55b7e7fcd7cced06f2d51bcd553bf30a3419774b
UbiOne <70473378+UbiDev-Discord@users.noreply.github.com>
2021-03-03 20:10:07 +0100

    update4
    funkcióbővítés
    hibajavítások

--- password.py
+++ password.py
@@ -1,12 +1,70 @@
 #created by UbiOne
 #Project Bela
-#Module 1 - Log in system
+#Version 1 - Log in system
+
+user = "0"
+logedinas1 = "\n\n\n\n\n\n\n\n\n\n\nBejelentkezve, mint UbiOne!"
+logedinas2 = "\n\n\n\n\n\n\n\n\n\n\nBejelentkezve, mint Musk3t4sl4c1!"
 
 print("Béla betöltve!")
 print("Kérlek jelentkezz be!")
 
-in_pass = input()
-if in_pass == "Bélalog8654":
-    print("Sikeres bejelentkezés!")
-    print("")
-    #Req Module 2 - Action panel
+print("Felhasználónév:")
+in_username = input()
+
+if in_username == "UbiOne":
+    print("Üdv újra, UbiOne!")
+    print("Jelszó:")
+    user = "1"
+
+if in_username == "Musk3t4sl4c1":
+    print("Üdv újra, Musk3t4sl4c1!")
+    print("Jelszó:")
+    user = "2"
+
+
+if user == "1":
+    ubione_pass = input()
+    if ubione_pass == "UbiOne7240":
+        print(logedinas1)
+
+if user == "2":
+    musk3t4sl4c1_pass = input()
+    if musk3t4sl4c1_pass == "Ho10La10":
+        print(logedinas2)
+
+
+logout = input()
+
+if logout == "Logout":
+    if user == "1":
+        user = "0"
+    if user == "2":
+        user = "0"
+
+if user == "0":
+    print("Kérlek jelentkezz be!")
+
+print("Felhasználónév:")
+in_username = input()
+
+if in_username == "UbiOne":
+    print("Üdv újra, UbiOne!")
+    print("Jelszó:")
+    user = "1"
+
+if in_username == "Musk3t4sl4c1":
+    print("Üdv újra, Musk3t4sl4c1!")
+    print("Jelszó:")
+    user = "2"
+
+
+if user == "1":
+    ubione_pass = input()
+    if ubione_pass == "UbiOne7240":
+        print("Bejelentkezve, mint UbiOne!")
+
+if user == "2":
+    musk3t4sl4c1_pass = input()
+    if musk3t4sl4c1_pass == "Ho10La10":
+        print("Bejelentkezve, mint Musk3t4sl4c1!")
