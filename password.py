d37977e7910e0e20e9e8424f2f8c1f45b2430aab
UbiOne <70473378+UbiDev-Discord@users.noreply.github.com>
2021-03-03 19:25:21 +0100

    project-bela update1
    Hozzáadtam a bejelentkezés folyamatot

---
+++ password.py
@@ -0,0 +1,11 @@
+#created by UbiOne
+#Project Bela
+#Module 1 - Log in system
+
+print("Béla loaded!")
+print("Please log in!")
+
+in_pass = input()
+if in_pass == "Bélalog8654":
+    print("Login succes!")
+    #Req Module 2 - Action panel
