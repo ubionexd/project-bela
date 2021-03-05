edd884199bcbf16db73ef898630bf4703a006e68
UbiOne <koviubi32.busines@gmail.com>
2021-03-05 15:48:00 +0100

    update5
    Felolvasás
    Hibajavítások

--- password.py
+++ password.py
@@ -2,36 +2,48 @@
 #Project Bela
 #Version 1 - Log in system
 
+import playsound
+
 user = "0"
 logedinas1 = "\n\n\n\n\n\n\n\n\n\n\nBejelentkezve, mint UbiOne!"
 logedinas2 = "\n\n\n\n\n\n\n\n\n\n\nBejelentkezve, mint Musk3t4sl4c1!"
 
+print("Betöltés...")
+
+playsound.playsound("loaded.mp3", True)
+
 print("Béla betöltve!")
 print("Kérlek jelentkezz be!")
-
 print("Felhasználónév:")
+
+playsound.playsound("loginpls.mp3", True)
+
 in_username = input()
 
 if in_username == "UbiOne":
     print("Üdv újra, UbiOne!")
     print("Jelszó:")
     user = "1"
+    playsound.playsound("ubionepass.mp3", True)
 
 if in_username == "Musk3t4sl4c1":
     print("Üdv újra, Musk3t4sl4c1!")
     print("Jelszó:")
     user = "2"
+    playsound.playsound("musk3t4sl4c1pass.mp3", True)
 
 
 if user == "1":
     ubione_pass = input()
     if ubione_pass == "UbiOne7240":
         print(logedinas1)
+        playsound.playsound("succes-login.mp3", True)
 
 if user == "2":
     musk3t4sl4c1_pass = input()
     if musk3t4sl4c1_pass == "Ho10La10":
         print(logedinas2)
+        playsound.playsound("succes-login.mp3", True)
 
 
 logout = input()
@@ -39,8 +51,10 @@ logout = input()
 if logout == "Logout":
     if user == "1":
         user = "0"
+        playsound.playsound("succes-logout.mp3", True)
     if user == "2":
         user = "0"
+        playsound.playsound("succes-logout.mp3", True)
 
 if user == "0":
     print("Kérlek jelentkezz be!")
