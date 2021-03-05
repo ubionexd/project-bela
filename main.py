8fa04e19c2c28c18845b33eb541a3f30242202a1
UbiOne <koviubi32.busines@gmail.com>
2021-03-05 18:53:10 +0100

    update8
    xd

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ if in_username == "Musk3t4sl4c1":
     user = "2"
     playsound.playsound("musk3t4sl4c1pass.mp3", True)
 
+else: print("Nem található ilyen parancs.")
+
 
 if user == "1":
     ubione_pass = input()
@@ -66,30 +68,3 @@ if logout == "Logout":
         print("Kijelentkezés...")
         playsound.playsound("succes-logout.mp3", True)
         print(space)
-
-if user == "0":
-    print("Kérlek jelentkezz be!")
-
-print("Felhasználónév:")
-in_username = input()
-
-if in_username == "UbiOne":
-    print("Üdv újra, UbiOne!")
-    print("Jelszó:")
-    user = "1"
-
-if in_username == "Musk3t4sl4c1":
-    print("Üdv újra, Musk3t4sl4c1!")
-    print("Jelszó:")
-    user = "2"
-
-
-if user == "1":
-    ubione_pass = input()
-    if ubione_pass == "UbiOne7240":
-        print("Bejelentkezve, mint UbiOne!")
-
-if user == "2":
-    musk3t4sl4c1_pass = input()
-    if musk3t4sl4c1_pass == "Ho10La10":
-        print("Bejelentkezve, mint Musk3t4sl4c1!")
